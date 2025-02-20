6d966c55fb79b90290fca93240184bbb6e38b0da
nomannhohar <aliyounas084@gmail.com>
2025-02-21 03:20:39 +0500

    func: added code of EditURL for GoTinyURL

--- api/routes/editUrl.go
+++ api/routes/editUrl.go
@@ -1,1 +1,49 @@
 package routes
+
+import (
+	"net/http"
+	"time"
+
+	"github.com/gin-gonic/gin"
+	"github.com/nomankhokhar/GoTonyUrl/api/database"
+	"github.com/nomankhokhar/GoTonyUrl/api/models"
+)
+
+func EditURL(c *gin.Context) {
+	shortID := c.Param("shortID")
+	var body models.Request
+
+	if err := c.ShouldBind(&body); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"error": "Cacnnot Parse JSON",
+		})
+	}
+
+	r := database.CreateClient(0)
+	defer r.Close()
+
+	// check if the shortID exists in the DB or not
+
+	val, err := r.Get(database.Ctx, shortID).Result()
+
+	if err != nil || val == "" {
+		c.JSON(http.StatusNotFound, gin.H{
+			"error": "ShortID does't exists",
+		})
+	}
+
+	// Update the content of the URL, expiry time with the shortID
+
+	err = r.Set(database.Ctx, shortID, body.URL, body.Expiry*3600*time.Second).Err()
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{
+			"error": "Unable to update the shortend content",
+		})
+		return
+	}
+
+	c.JSON(http.StatusOK, gin.H{
+		"message": "The content has been Updated !!!!",
+	})
+
+}
